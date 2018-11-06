9820d58217c822326c6740d48296c6afc8476f10
Jacob Hinkston <jacobhinkston@gmail.com>
2018-11-06 01:16:58 -0700

    Added more signup functionality in the areas where edge cases could create problems in the future.

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,52 +1,85 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
-import './Signup.css'
-import { firebase } from '../../firebase-config.js'
-import api from '../../api'
-import gifLoading from '../../assets/uploading.gif'
+import './Signup.css';
+import { firebase } from '../../firebase-config.js';
+import api from '../../api';
+import gifLoading from '../../assets/uploading.gif';
+import memeEmoji from '../../assets/crazy_crying_emoji.png';
 export default class Signup extends Component {
     constructor(props){
-        super(props)
-        this.state={
+        super(props);
+        this.state = {
             signingUp: false,
             signedUp: undefined,
             email: undefined,
             displayName: undefined,
-            signupErr: ""
+
+            signupErr: [],
+            err_password: undefined,
+            err_displayName: undefined,
+
+        };
+        this.onSignupSubmit = this.onSignupSubmit.bind(this);
+        this.testIfUserExists = this.testIfUserExists.bind(this);
+        this.testIfPasswordsMatch = this.testIfPasswordsMatch.bind(this);
+        this.resetDisplayNameErr = this.resetDisplayNameErr.bind(this);
+        this.resetPasswordErr = this.resetPasswordErr.bind(this);
+    }
+    testIfUserExists(displayName){
+        //TODO, Make syncronous with firebase signup so that it catches bad input.
+        fetch(api.userInfo_Data+`/${displayName}`)
+            .then(res => res.json())
+            .then(resJSON => {
+                this.setState({
+                    err_displayName: ( resJSON.user  ? true : false )
+                })
+            })
+    }
+    testIfPasswordsMatch(password1, password2){
+        this.setState({
+            err_password: password1 !== password2
+        })
+    }
+    resetDisplayNameErr(){
+        if(this.state.err_displayName){
+            this.setState({ err_displayName: undefined });
         }
-        this.onSignupSubmit = this.onSignupSubmit.bind(this)
+    }
+    resetPasswordErr(){
+        if(this.state.err_password){
+            this.setState({ err_password: undefined })
+        }
     }
     onSignupSubmit(event){
-        event.preventDefault()
-        this.setState({signingUp: true})
-        var formData = new FormData(event.target)
-        var signUpCredentials={
+        event.preventDefault();
+        let formData = new FormData(event.target);
+        let signUpCredentials = {
             displayName: formData.get('displayName'),
             email: formData.get('email'),
-            profilePicture: event.target[1].files[0],
-            password: formData.get('password1'),
+            password1: formData.get('password1'),
             password2: formData.get('password2')
-        }
-        if(signUpCredentials.password != signUpCredentials.password2){
-            alert('Your passwords do not match, please try again.')
-        }else{
+        };
+        this.testIfUserExists();
+        this.testIfPasswordsMatch(
+            signUpCredentials.password1,
+            signUpCredentials.password2
+        );
+            this.setState({signingUp: true});
+
             firebase.auth().createUserWithEmailAndPassword(
                 signUpCredentials.email,
-                signUpCredentials.password
+                signUpCredentials.password1
             )
                 .then(res => {
                     if(res.additionalUserInfo.isNewUser){
                         var user = firebase.auth().currentUser;
                         user.sendEmailVerification()
-                            .then(() => {
-
-                            })
+                            .then(() => {})
                             .catch((error) => {
                                 this.setState({
-                                    signupErr: this.state.signupErr + "\n" + error
-                                })
+                                    signupErr: [...this.state.signupErr, JSON.stringify(error)]
+                                });
                             });
-                        
                         firebase.auth().currentUser.updateProfile({
                             displayName: signUpCredentials.displayName
                         })
@@ -54,23 +87,21 @@ export default class Signup extends Component {
                                 this.createUser({
                                     displayName: signUpCredentials.displayName,
                                     profilePictureUrl: signUpCredentials.photoUrl
-                                })
+                                });
                                 this.setState({
                                     displayName: signUpCredentials.displayName,
                                     email: signUpCredentials.displayName
-                                })
-                                firebase.auth().signOut()
+                                });
+                                firebase.auth().signOut();
                             })
                     }
                 })
                 .catch(error => {
-                    var errorCode = error.code;
-                    var errorMessage = error.message;
-                    console.error(errorCode)
                     this.setState({
-                        signupErr: this.state.signupErr + "\n" + error
-                    })
-                    alert(errorMessage)
+                        signupErr: [...this.state.signupErr, error.message],
+                        signedUp: false,
+                        signingUp: false
+                    });
                 });
         }
     }
@@ -79,27 +110,26 @@ export default class Signup extends Component {
             method: 'POST',
             body: JSON.stringify(signUpCredentials),
             headers: new Headers({ 'Content-type': 'application/json'})
-        }
+        };
         fetch(api.userInfo_Data, postOptions)
             .then(res => {
                 if(res.status===404 || res.status ===500){
-                    alert('Oops, something went wrong. User could not be created.')
+                    alert('Oops, something went wrong. User could not be created.');
                     this.setState({
                         signedUp: false,
                         signingUp: false,
-                        database_ERR: true,
-                        signupErr: this.state.signupErr + "\n" + res
-                    })
+                        signupErr: [...this.state.signupErr, toString(res)]
+                    });
                 }
                 else{
                     this.setState({ 
                         signedUp: true,
                         signingUp: false
-                    })
+                    });
                 }
                 
             })
-            .catch(error => console.error(error))
+            .catch(error => console.error(error));
     }
 
     render() {
@@ -136,42 +166,35 @@ export default class Signup extends Component {
                         onSubmit={this.onSignupSubmit}
                         disabled={this.state.signingUp}
                     >
-                        {/*TODO: Optional, but you can implement this later in another way (/Profile) perhaps?*/}
-                        {/*<div>*/}
-                            {/*<label>First name:</label>*/}
-                            {/*<input*/}
-                                {/*autoFocus*/}
-                                {/*required*/}
-                                {/*type="text"*/}
-                                {/*name="fistName"*/}
-                            {/*/>*/}
-                        {/*</div>*/}
-                        {/*<div>*/}
-                            {/*<label>Last name:</label>*/}
-                            {/*<input*/}
-                                {/*required*/}
-                                {/*type="text"*/}
-                                {/*name="lastName"*/}
-                            {/*/>*/}
-                        {/*</div>*/}
                         <div>
                             <label>Display name:</label>
                             <input
                                 required
+                                onChange={this.resetDisplayNameErr}
                                 type="text"
                                 name="displayName"
                                 disabled={this.state.signingUp}
                             />
+                            <p
+                                className={
+                                    "signup-form-err" + (this.state.err_displayName ? "" : " hidden")
+                                }>
+                                {(
+                                    this.state.err_displayName ?
+                                        "❌ This display name is already taken." :
+                                        null
+                                )}
+                            </p>
                         </div>
-                        <div>
-                            <label>Profile picture: [Disabled]</label>
-                            <input
-                                disabled
-                                type="file"
-                                name="profilePicture"
-                                disabled={this.state.signingUp}
-                            />
-                        </div>
+                        {/*<div>*/}
+                            {/*<label>Profile picture: [Disabled]</label>*/}
+                            {/*<input*/}
+                                {/*disabled*/}
+                                {/*type="file"*/}
+                                {/*name="profilePicture"*/}
+                                {/*disabled={this.state.signingUp}*/}
+                            {/*/>*/}
+                        {/*</div>*/}
                         <div>
                             <label>Email:</label>
                             <input
@@ -195,24 +218,41 @@ export default class Signup extends Component {
                             <label>Re-enter Password:</label>
                             <input
                                 required
+                                onChange={this.resetPasswordErr}
                                 type="password"
                                 name="password2"
                                 disabled={this.state.signingUp}
                             />
+                            <p
+                                className={
+                                    "signup-form-err" + (this.state.err_password ? "" : " hidden")
+                                }>
+                                {(
+                                    this.state.err_password ?
+                                        "❌ Passwords must match." :
+                                        null
+                                )}
+                            </p>
                         </div>
                         {signupBtn}
                     </form>
                 </React.Fragment>
             )
-        }else if(this.state.signedUp == false || this.state.signupErr!=""){
+        }else if(this.state.signedUp == false || this.state.signupErr.length != 0){
+            console.log()
+            let errors = this.state.signupErr.map(err => {
+                return(<p>- {err} </p>)
+            });
             signUp = (
                 <div className="signup-status">
-                    <h1>Oops, this is embarrasing... </h1>
+                    <img src={memeEmoji}/>
+                    <h1>Oops, this is embarrassing... </h1>
                     <h3>There apears to have been a problem creating this account.</h3>
-                    <h4>{"Error(s): " + this.state.signupErr}</h4>
+                    <div className="signup-errors">
+                        {errors}
+                    </div>
                 </div>
-            )
-            
+            );
         }else{
             signUp = (
                 <div className="signup-status">
